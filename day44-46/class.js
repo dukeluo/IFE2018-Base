cabd103453cfaa3712841b86c236b6d22d2bf997
duke <dukeluo@outlook.com>
2018-12-15 21:51:51 +0800

    update class.js

--- day44-46/class.js
+++ day44-46/class.js
@@ -8,8 +8,8 @@ function Restaurant(cash, seat, staff) {
     this.cash = cash;
     this.seat = seat;
     this.staff = staff;
-    this.bookedMeal = [];
-    this.orderList = [];
+    this.mealQueue = [];
+    this.orderQueue = [];
 }
 
 /**
@@ -24,36 +24,66 @@ Restaurant.prototype.recruitStaff = function (s) {
 };
 
 /**
- * @param  {number} id
+ * @param  {Staff} s
  * @return {void}
  */
-Restaurant.prototype.fireStaff = function (id) {
-    if (typeof id !== "number") {
+
+Restaurant.prototype.fireStaff = function (s) {
+    if (!(s instanceof Staff)) {
         return ;
     }
+
     this.staff = this.staff.filter(function (item, index, array) {
-        return item.id !== id;
+        return !item.isSame(s);
     });
 };
 
 /**
- * @param  {Order} o
- * @return {void}
+ * @return {number} the id of the processing order
  */
-Restaurant.prototype.beginOrder = function (o) {
-    this.bookedMeal = this.bookedMeal.concat(o.mealList);
-    this.cash -= o.cost;
-    this.seat -= o.seat;
-    this.orderList.push(o);
+Restaurant.prototype.beginOrder = function () {
+    var targetOrder = this.orderQueue.shift(),
+        mealList = targetOrder.mealList,
+        processingMealObject,
+        i;
+
+    this.cash -= targetOrder.cost;
+    this.seat -= targetOrder.seat;
+    for (i = 0; i < mealList.length; i++) {
+        processingMealObject = {};
+        processingMealObject.meal = mealList[i];
+        processingMealObject.customer = customerId;
+        this.orderQueue.push(processingMealObject);
+    }
+    return targetOrder.id;
 }
 
 /**
- * @param  {Order} o
+ *
+ * @param  {number} orderID
  * @return {void}
  */
-Restaurant.prototype.finishOrder = function (o) {
-    this.cash += o.price;
-    this.seat += o.seat;
+Restaurant.prototype.finishOrder = function (orderID) {
+    var targetOrder,
+        i;
+
+    if (typeof +orderID !== "number") {
+        return ;
+    }
+    for (i = 0; i < this.orderQueue.length; i++) {
+        if (orderID === this.orderQueue[i].id) {
+            targetOrder = this.orderQueue[i];
+            break;
+        }
+    }
+    if (!targetOrder) {
+        console.error("No this order: " + orderID);
+    }
+    this.cash += targetOrder.price;
+    this.seat += targetOrder.seat;
+    this.orderQueue = this.orderQueue.filter(function (item, index, array) {
+        return !item.isSame(targetOrder);
+    });
 }
 
 
@@ -69,6 +99,14 @@ function Staff(id, name, salary) {
     this.salary = salary;
 }
 
+/**
+ * @param  {Staff} s
+ * @return {boolean}
+ */
+Staff.prototype.isSame = function (s) {
+    return areObjectsSame(this, s);
+}
+
 
 /**
  * @param       {number} id
@@ -78,7 +116,6 @@ function Staff(id, name, salary) {
  */
 function Waiter(id, name, salary) {
     Staff.call(this, id, name, salary);
-
 }
 
 Waiter.prototype = Object.create(Staff.prototype);
@@ -93,9 +130,9 @@ Waiter.prototype.constructor = Waiter;
  * @return {void}
  */
 Waiter.prototype.submitOrder = function (customerId, seat, mealList, r) {
-    var o = new Order(Date.now(), mealList, seat, mealList);
+    var newOrder = new Order(Date.now(), mealList, seat, customerId);
 
-    r.beginOrder(o);
+    r.orderQueue.push(newOrder);
 };
 // TODO:
 Waiter.prototype.serve = function (customerId) {
@@ -132,11 +169,11 @@ function Customer(id, seat, mealList) {
     this.mealList = mealList;
 }
 
-Customer.prototype.order = function (w) {
-    if (!(w instanceof Waiter)) {
+Customer.prototype.orderMeals = function (w, r) {
+    if (!(w instanceof Waiter) || !(r instanceof Restaurant)) {
         return ;
     }
-    return w.submitOrder(this.id, this.seat, this.mealList);
+    return w.submitOrder(this.id, this.seat, this.mealList, r);
 };
 // TODO:
 Customer.prototype.eat = function (r, m) {
@@ -158,7 +195,7 @@ function Meal(name, cost, price) {
 
 /**
  * @param       {number} id
- * @param       {Array} mealList
+ * @param       {Array} mealList    an array of Meal
  * @param       {number} seat
  * @param       {number} customerId
  * @constructor
@@ -176,14 +213,46 @@ function Order(id, mealList, seat, customerId) {
     });
 }
 
+/**
+ *
+ * @param  {order} o
+ * @return {boolean}
+ */
+Order.prototype.isSame = function (o) {
+    return areObjectsSame(this, o);
+}
 
+
+/**
+ * Helper Function
+ * Determine if the two objects are the same
+ * @param  {Object} a
+ * @param  {Object} b
+ * @return {boolean}
+ */
+function areObjectsSame(a, b) {
+    var keys = Object.keys(a),
+        length = keys.length,
+        key;
+
+    if (Object.keys(b).length !== length) {
+        return false;
+    }
+    while (length--) {
+        key = keys[length];
+        if (!(b.hasOwnProperty(key)) && b.key !== a.key) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // var ifeRestaurant = new Restaurant(1000000, 20, []);
 // var newCook = new Chef(1, "Tony", 10000);
-// console.log(newCook.constructor);
 //
-// console.log(ifeRestaurant.staff);
+// console.log(newCook.constructor);     // [Function: Chef]
+// console.log(ifeRestaurant.staff);     // []
 // ifeRestaurant.recruitStaff(newCook);
-// console.log(ifeRestaurant.staff);
-// ifeRestaurant.fireStaff(newCook.id);
-// console.log(ifeRestaurant.staff);
+// console.log(ifeRestaurant.staff);     // [ Chef { id: 1, name: 'Tony', salary: 10000 } ]
+// ifeRestaurant.fireStaff(newCook);
+// console.log(ifeRestaurant.staff);     // []
